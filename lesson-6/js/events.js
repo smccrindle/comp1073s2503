774d41461245c9b7a45e82a4ec5522900140ba7e
Scott McCrindle <iainmccrindle@gmail.com>
2025-06-10 10:09:40 -0400

    Completed all JS and HTML changes for the simple events part of the lesson.

--- lesson-6/js/events.js
+++ lesson-6/js/events.js
@@ -7,28 +7,34 @@ function random(number) {
 }
 function bgChange() {
     let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    console.log(rndCol);
     document.body.style.backgroundColor = rndCol;
 }
 // Event Listeners/Handlers
 btn.onclick = bgChange;
 
-
 /* STEP 1: Experiment with a variety of different events - comment out the above onclick listener and build an alternate one below - try the following - onfocus/onblur, ondblclick, onmouseover/onmouseout, window.onkeypress/onkeydown/onkeyup
 */
 
 
-/* STEP 2: Inline event handlers - don't use! */
+/* STEP 2: Inline event handlers - use with caution */
 // Comment out the above event listener/handler, and add the same event as an attribute directly to the button element
 
 /* STEP 3: Let's apply event handlers/listeners to a group of buttons - comment out the above BUTTON element, and create three new ones, A, B, and C */
-
-
+const buttons = document.querySelectorAll("button");
+for (let i = 0; i < buttons.length; i ++) {
+    //buttons[i].onclick = bgChange;
+    buttons[i].addEventListener("click", bgChange);
+};
+buttons[1].removeEventListener("click", bgChange);
 /* STEP 4a: addEventListener() and removeEventListener()
 Let's rewrite the above code with these two methods - comment out the A, B, C buttons and restore the original button - then comment out the above code */
 
 
 /* STEP 4b: We can also put the entire bgChange() function inside an anonymous function, if we'd like: */
-
+buttons[1].addEventListener("click", function() {
+    document.body.style.backgroundColor = "rgb(0, 0, 0)";
+});
 
 
 /* STEP 4c: We can now remove the event listener … comment out the above code in STEP 4b, and then add another event listener, then remove it (silly, yes, but try it all the same) */
